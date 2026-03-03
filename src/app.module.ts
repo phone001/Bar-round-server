a575756723813a211e6455c8c0deb92eee3223e1
gyeon <rlarlgus6077@hanmail.net>
2026-03-03 23:40:27 +0900

    [feat] 상품 리소스 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,11 +7,12 @@ import { WinstonModule } from 'nest-winston';
 import * as winston from 'winston';
 import { loggerFormat } from 'util/format/text-format';
 import { OrderModule } from './order/order.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
   imports: [BarModule, CommonModule,WinstonModule.forRoot({
     transports:[new winston.transports.Console({format:winston.format.combine(winston.format.timestamp({format:'YYYY-MM-DD HH:mm:ss'}),loggerFormat)})]
-  }), OrderModule],
+  }), OrderModule,ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
